e8e64c9c346b0df794b0ef227aee02f2798a4f37
Felix Wenzel <felixwenzel0@gmail.com>
2022-01-19 00:17:32 +0100

    widest peak

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from z3 import *
 
-"""
-In computer science, the subset sum problem is an important decision problem in complexity theory
-and cryptography. There are several equivalent formulations of the problem. One of them is: given a
-set (or multiset) of integers, is there a non-empty subset whose sum is zero? For example, given the
-set {−7, −3, −2, 9000, 5, 8}, the answer is yes because the subset {−3, −2, 5} sums to zero.
-"""
-
 
 def zerosum(xs):
+    """
+    In computer science, the subset sum problem is an important decision problem in complexity theory
+    and cryptography. There are several equivalent formulations of the problem. One of them is: given a
+    set (or multiset) of integers, is there a non-empty subset whose sum is zero? For example, given the
+    set {−7, −3, −2, 9000, 5, 8}, the answer is yes because the subset {−3, −2, 5} sums to zero.
+    """
     n = len(xs)
 
     ss = [Bool(f"ss[{i}]") for i in range(n)]
@@ -28,15 +27,11 @@ def zerosum(xs):
         print("No solution")
 
 
-zerosum([-7, 7, -3, -2, -3, 9000, 5, 8])
-
-"""
-Given a list of items which might be packed, each of which has a weight and a value (both non-negative real
-numbers), pick the items with the greatest total value that do not exceed a given capacity of the knapsack w.
-"""
-
-
 def kn(ws, vs, M):
+    """
+    Given a list of items which might be packed, each of which has a weight and a value (both non-negative real
+    numbers), pick the items with the greatest total value that do not exceed a given capacity of the knapsack w.
+    """
     n = len(ws)
 
     ns = [Int(f"ns[{i}]") for i in range(n)]
@@ -53,15 +48,11 @@ def kn(ws, vs, M):
         print("No solution")
 
 
-kn([1, 2, 3], [1, 3, 5], 8)
-
-"""
-Given a list of integers, find the (contiguous) segment that has maximal sum. E.g.: the maximum sum segment
-of [2, -3, 4, -1, 3] is [4, -1, 3].
-"""
-
-
 def mss(xs):
+    """
+    Given a list of integers, find the (contiguous) segment that has maximal sum. E.g.: the maximum sum segment
+    of [2, -3, 4, -1, 3] is [4, -1, 3].
+    """
     n = len(xs)
 
     l, r = Ints("l r")
@@ -79,10 +70,6 @@ def mss(xs):
         print("No solution")
 
 
-mss([0, -2, 5, 5, -7, 2])
-
-
-# Bonus: sort with duplicates
 def sort(xs):
     n = len(xs)
 
@@ -108,34 +95,41 @@ def sort(xs):
         print("No solution")
 
 
-sort([9, 8, 2, 3, 5, 6, 12, 9, 9])
-
-"""
-Given a list, a peak is a subsegment composed of an increasing segment, directly followed by a decreasing one.
-Find a widest peak of a given list. For example, the widest peak of [3, 2, 1, 2, 3, 2, 1, 2] is [1, 2, 3, 2, 1].
-"""
-
-
 def wp(xs):
+    """
+    Given a list, a peak is a subsegment composed of an increasing segment, directly followed by a decreasing one.
+    Find a widest peak of a given list. For example, the widest peak of [3, 2, 1, 2, 3, 2, 1, 2] is [1, 2, 3, 2, 1].
+    """
     n = len(xs)
     s = Optimize()
-    l, p, r = Ints("l p r")
+    ns = [Bool(f"ns[{i}]") for i in range(n)]
 
-    ascending = [If(xs[i] < xs[i + 1], 1, 0) for i in range(n - 1)]
-    print(ascending)
-    descending = [If(xs[i - 1] < xs[i], 1, 0) for i in range(1, n)]
-    print(descending)
-    max = r - l
-    print(max)
+    edge_valleys = [If(xs[0] < xs[1], True, False), If(xs[n - 1] < xs[n - 2], True, False)]
+    valleys = [If(And(xs[i] < xs[i - 1], xs[i] < xs[i + 1]), True, False) for i in range(1, n - 1)]
+    print([edge_valleys[0]] + valleys + [edge_valleys[1]])
 
 
-wp([1, 2, 1, 3])
+    maximizer = [Sum([ns[i] for i in range(n)])]
 
-"""
-In a list, a “valley” is a segment in which all the elements are at most equal to the ends (the “walls”). Find a
-longest valley. For example, the longest valley in the list [3, 2, 1, 2, 3, 2, 1, 2] is [3, 2, 1, 2, 3]
-"""
+    s.
+
+    if s.check() == sat:
+        m = s.model()
+        print([m.eval(ns[i]) for i in range(n)])
+    else:
+        print("No solution")
 
 
 def wv():
+    """
+    In a list, a “valley” is a segment in which all the elements are at most equal to the ends (the “walls”). Find a
+    longest valley. For example, the longest valley in the list [3, 2, 1, 2, 3, 2, 1, 2] is [3, 2, 1, 2, 3]
+    """
     pass
+
+
+# zerosum([-7, 7, -3, -2, -3, 9000, 5, 8])
+# kn([1, 2, 3], [1, 3, 5], 8)
+# mss([0, -2, 5, 5, -7, 2])
+# sort([9, 8, 2, 3, 5, 6, 12, 9, 9])
+wp([1, 2, 4, 3, 1, 3])
